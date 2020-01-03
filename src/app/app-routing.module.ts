394a07dbd7ca6753eb4a0ae7d795ed875ec5bbce
antic <santic16@raf.rs>
2020-01-03 17:19:01 +0100

    Set /machine as home path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AuthGuardService} from './core/services/auth-guard.service';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {TokenInterceptor} from './core/interceptors/token.interceptor';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'machines',
+    pathMatch: 'full'
+  },
   {
     path: 'machines',
     loadChildren: './machine/machine.module#MachineModule',
